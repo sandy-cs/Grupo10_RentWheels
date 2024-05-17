713140ecfadded357eec3e564bb188053a19c705
Lucas Henrique <126213423+Frianitrir@users.noreply.github.com>
2024-05-16 22:09:03 -0300

    atualizando arquivos

---
+++ JavaScript.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    // Inicia o carrossel
+    $('.carousel').carousel();
+
+    // Define o intervalo para mudança de slide
+    $('.carousel').carousel({
+        interval: 1000 // Tempo 1 segundo
+    });
+});
+
